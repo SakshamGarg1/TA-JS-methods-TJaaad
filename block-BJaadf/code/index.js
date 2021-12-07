42fa4908a56847c6dbe9987289110573b21327da
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2021-12-07 17:58:45 +0530

    Update index.js

--- block-BJaadf/code/index.js
+++ block-BJaadf/code/index.js
@@ -74,23 +74,37 @@ let numbersTwo = [1, 2, [3, 4, [5, 6]]];
 let numbersThree = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
 
 // Convert the numbers array to one level element. Remove sub-arrays to individual elements and log them
-
+console.log(number.flat(2));
 
 // Convert the numbersTwo array to one level element. Remove all sub-arrays to individual elements and log them.
 // (for multiple level flat accepts argument by default it's 1 `array.flat(2)`)
+console.log(numberTwo.flat(2));
+
 
 // Convert the numbersThree array to one level element.
+console.log(numberThree.flat(Infinity));
+
 // Remove all sub-arrays to individual elements, log them and update the value of numbersThree to the new flat array.
+numberThree= (numberThree.flat(Infinity);
+
 
 // Use forEach to log all the elements of numberThree array
+numberThree=console.log((num)=>console.log(num));
 
 // Create a new variable named doubleNumbers that store the numberThree array (each element should be multiplied by 2). Use map
+let doubleNumbers=doubleName.map((elm)=>elm*2);                                                                            
 
 // Create a new variable named tripleNumbers that store the numberThree array (each element should be multiplied by 3). Use map
+let tripleNumbers=tripleName.map((elm)=>elm*3);                                                                            
+
 
 // Create a new variable named halfNumbers that store the numberThree array (each element should be divided by 2). Use map
+let halfNumbers=halfName.map((elm)=>elm/2);                                                                            
+
 
 // Create a new variable named oddNumbers that store all the odd numbers in numbersThree array
+let oddNumbers=oddNumber.fil(
+(elm)=>elm!==2);
 
 // Create a new variable named evenNumbers that store all the even numbers in numbersThree array
 
